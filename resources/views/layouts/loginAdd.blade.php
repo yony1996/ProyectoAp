2e978a1dfdaa033b2768d0befd692b175cc2412b
yony <yonyvenegas@gmail.com>
2021-06-02 20:27:19 -0500

    2-06-2021-3
    Signed-off-by: yony <yonyvenegas@gmail.com>

--- resources/views/layouts/loginAdd.blade.php
+++ resources/views/layouts/loginAdd.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <title>AP Salud</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/vnd.microsoft.icon" href="{{asset('dists/assets/img/logo.ico')}}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 

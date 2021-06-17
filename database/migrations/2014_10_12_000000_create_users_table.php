91a3a5396f028bdf99bebe3acfacda2e6d4132ac
yony1996 <yonyvenegas@gmail.com>
2021-06-16 20:04:06 -0500

    database-custom

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
            $table->boolean('status')->default(1);
            $table->string('password',255);
-            $table->apiToken();
             $table->timestamps();
         });
     }

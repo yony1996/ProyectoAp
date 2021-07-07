69f788aceddc75bf96c767936c99b52c35bd0ad3
yony1996 <yonyvenegas@gmail.com>
2021-07-06 19:51:43 -0500

    fix autologin

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -14,71 +14,80 @@ use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
 
-    public function login(Request $request){
-        $credentials=$request->only('email','password');
+	public function login(Request $request){
+		$credentials=$request->only('email','password');
 
-        if(Auth::attempt($credentials)){
-            $user = Auth::user();
-            $passport=  $user->createToken(env('APP_NAME'))-> accessToken;
-            $rol=$user->getRoleNames()->first();
-            $success=true;
+		if(Auth::attempt($credentials)){
+			$user = Auth::user();
+			$passport=  $user->createToken(env('APP_NAME'))-> accessToken;
+			$rol=$user->getRoleNames()->first();
+			$success=true;
 
-            $user=Arr::add($user, 'rol',$rol);
+			$user=Arr::add($user, 'rol',$rol);
 
-            $data=compact('success','user','passport');
-            return $data;
-        }
-        else{
-            $success=false;
-            $message='algo ha fallado';
-            return compact('success','message');
-        }
-    }
+			$data=compact('success','user','passport');
+			return $data;
+		}
+		else{
+			$success=false;
+			$message='algo ha fallado';
+			return compact('success','message');
+		}
+	}
 
-    public function logout(Request $request){
-        $accessToken = Auth::user()->token();
-        $token= $request->user()->tokens->find($accessToken);
-        $token->revoke();
-        $success=true;
-        $message='se ha cerradon con éxito';
-        return compact('success','message');
-    }
+	public function logout(Request $request){
+		$accessToken = Auth::user()->token();
+		$token= $request->user()->tokens->find($accessToken);
+		$token->revoke();
+		$success=true;
+		$message='se ha cerradon con éxito';
+		return compact('success','message');
+	}
 
-    public function register(Request $request){
-        $rules = [
-            'name' => ['required', 'string', 'max:50'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8'],
-        ];
-        $this->validate($request, $rules);
+	public function register(Request $request){
+		$rules = [
+			'name' => ['required', 'string', 'max:50'],
+			'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+			'password' => ['required', 'string', 'min:8'],
+		];
+		$this->validate($request, $rules);
 
-        $user= User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+		$user= User::create([
+			'name' => $request->name,
+			'email' => $request->email,
+			'password' => Hash::make($request->password),
+		]);
 
-        Patient::create([
-            'ci'=>null,
-            'middle_name'=>'',
-            'last_name'=>'',
-            'second_last_name'=>'',
-            'phone'=>0,
-            'age'=>'',
-            'user_id'=>$user->id
-        ]);
-        $user->assignRole('paciente');
+		Patient::create([
+			'ci'=>null,
+			'middle_name'=>'',
+			'last_name'=>'',
+			'second_last_name'=>'',
+			'phone'=>'',
+			'age'=>'',
+			'user_id'=>$user->id
+		]);
+		$user->assignRole('paciente');
 
-        $credentials=$request->only(['email'=>$user['email'],'password'=>$user['password']]);
-        Auth::attempt($credentials);
-        $passport=  $user->createToken(env('APP_NAME'))-> accessToken;
-        $rol=$user->getRoleNames()->first();
-        $success=true;
+		$credentials=$request->only('email','password');
+		if (Auth::attempt($credentials)) {
+			$passport=  $user->createToken(env('APP_NAME'))-> accessToken;
+			$rol=$user->getRoleNames()->first();
+			$success=true;
 
-        $user=Arr::add($user, 'rol',$rol);
+			$user=Arr::add($user, 'rol',$rol);
 
-        $data=compact('success','user','passport');
-        return $data;
-    }
+			$data=compact('success','user','passport');
+			return $data;
+		}
+		$success=false;
+		$message='Ocurrio un error al loguearse';
+		$data=compact('success','message');
+		return $data;
+
+	}
+
 
 }
+
+
